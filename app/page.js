64619b909608a7751e76bd40ccffba05c346c712
nahid-dev <nahidv232@gmail.com>
2024-02-29 19:02:20 +0600

    navbar building done

--- app/page.js
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1 className="font-light text-4xl">Start Next.js Project</h1>
-    </div>
-  );
-}
---
+++ app/page.jsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div>
+      <h1 className="font-light text-4xl bg-gray-200">Start Next.js </h1>
+    </div>
+  );
+}
