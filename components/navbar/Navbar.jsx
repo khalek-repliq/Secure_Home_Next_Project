7e3066f4480925a812988a62e70433e29521c2dd
nahid-dev <nahidv232@gmail.com>
2024-03-01 17:13:12 +0600

    navbar update for moible responsive

--- components/navbar/Navbar.jsx
+++ components/navbar/Navbar.jsx
@@ -1,13 +1,15 @@
 "use client";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 import { usePathname } from "next/navigation";
 
+// Navbar Items style here
 const navStyle = {
   active: "bg-[#27bfb3] text-white  px-3 py-1 rounded-sm",
   default: "px-3 py-1 rounded-sm",
 };
 
+// All navbar Items list here
 const navItem = [
   {
     id: 1,
@@ -38,12 +40,13 @@ const navItem = [
 
 const Navbar = () => {
   const pathName = usePathname();
+  const [open, setOpen] = useState(false);
 
   const handleNavbarStyle = (itemPath) =>
     itemPath === pathName ? true : false;
 
   return (
-    <div className=" mb-6 shadow-lg py-5">
+    <div className=" mb-6 shadow-lg py-5 px-3 md:px-5 lg:px-10  bg-gray-300">
       <div className="flex justify-between ">
         {/* logo area */}
         <div>
@@ -73,8 +76,53 @@ const Navbar = () => {
         </div>
 
         {/* Items area */}
-        <div className=" leading-[2.5]">
-          <ul className=" flex items-center space-x-8 font-medium">
+        <div className=" md:leading-[2.5]">
+          {/* Mobile hamburger menu icon */}
+          <div className="md:hidden">
+            {/* menu icon */}
+            <div>
+              <svg
+                onClick={() => setOpen(!open)}
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6 mt-2 cursor-pointer"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+                />
+              </svg>
+            </div>
+            {/* mobile menu item list */}
+            <div>
+              <ul
+                className={`md:hidden items-center font-medium bg-white w-full absolute  top-[80px] border text-center space-y-5 transition-all duration-300 ${
+                  open ? "left-0" : "-left-full"
+                }`}
+              >
+                {navItem.map((item) => (
+                  <li key={item.id}>
+                    <Link
+                      className={
+                        handleNavbarStyle(item.path)
+                          ? navStyle.active
+                          : navStyle.default
+                      }
+                      href={item.path}
+                    >
+                      {item.name}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+
+          <ul className={`hidden md:flex items-center space-x-8 font-medium`}>
             {navItem.map((item) => (
               <li key={item.id}>
                 <Link
