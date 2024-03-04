c4400830205b7b8f2d84262f66fbfb2ebb71ed66
nahid-dev <nahidv232@gmail.com>
2024-03-04 16:12:46 +0600

    dummy product data initialization done

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,14 @@
 import HomeBanner from "@/components/home-banner/HomeBanner";
+import ProductsSection from "@/components/products-section/ProductsSection";
 
 export default function Home() {
   return (
     <div>
       {/* Home page banner area */}
       <HomeBanner></HomeBanner>
+
+      {/* Product display section */}
+      <ProductsSection></ProductsSection>
     </div>
   );
 }
---
+++ components/products-section/ProductsSection.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import products from "@/DummyData/products/products.json";
+
+const ProductsSection = () => {
+  console.log(products);
+  return (
+    <div>
+      <h1 className="text-4xl">Lorem ipsum dolor sit amet.</h1>
+    </div>
+  );
+};
+
+export default ProductsSection;
