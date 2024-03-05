866b1c627d267a90dd6d4073af645dc29cbda959
nahid-dev <nahidv232@gmail.com>
2024-03-05 19:06:38 +0600

    update product details page. show the data in details page.

--- app/products/[id]/page.jsx
+++ app/products/[id]/page.jsx
@@ -1,8 +1,45 @@
 "use client";
+import { useQuery } from "@tanstack/react-query";
+import Image from "next/image";
 import { useParams } from "next/navigation";
 
 export default function ProductDetails() {
   const { id } = useParams();
-  console.log(id);
-  return <h1 className="text-3xl">Product Details Page.</h1>;
+  const { data, isLoading } = useQuery({
+    queryKey: ["products"],
+    queryFn: () =>
+      axios
+        .get(
+          "https://raw.githubusercontent.com/khalek-repliq/Secure_Home_Next_Project/main/DummyData/products/products.json"
+        )
+        .then((res) => res.data),
+  });
+  const product = data?.find((product) => product.id == id);
+  return (
+    <>
+      <div>
+        <div>
+          <div className="grid grid-cols-1 lg:grid-cols-2">
+            {/* Product Image Area */}
+            <div>
+              <div className="border w-96 h-96">
+                <Image
+                  src={product?.image}
+                  alt="Product Image"
+                  width={350}
+                  height={300}
+                />
+              </div>
+            </div>
+            {/* Product Specification Content */}
+            <div>
+              <div>
+                <h4 className="text-4xl">{product?.title}</h4>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
