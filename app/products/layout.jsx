db834b3b79d85398fe436cd6f256e07278bb91f5
nahid-dev <nahidv232@gmail.com>
2024-03-08 13:08:20 +0600

    servies json file created

--- app/products/layout.jsx
+++ app/products/layout.jsx
@@ -1,12 +1,21 @@
+"use client";
 import PageHeader from "@/components/page-header/PageHeader";
+import { usePathname } from "next/navigation";
 
 export default function ProductLayout({ children }) {
+  const pathName = usePathname();
+  console.log(pathName);
+
+  const showPageHeader = pathName === "/products";
+
   return (
     <div>
-      <PageHeader
-        title={"All Products"}
-        subTitle={"Choose your desire product for secure your home."}
-      />
+      {showPageHeader && (
+        <PageHeader
+          title={"All Products"}
+          subTitle={"Choose your desire product for secure your home."}
+        />
+      )}
       <div className="px-3 md:px-5 lg:px-10">{children}</div>
     </div>
   );
