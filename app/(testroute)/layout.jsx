a2d31383ffc73f2f0d61aabc7ab9c1797b1f435e
Md Tanjil <125866000+tanjil-repliq@users.noreply.github.com>
2024-03-09 14:01:01 +0600

    Added demo group layout to root directory

---
+++ app/(testroute)/layout.jsx
@@ -0,0 +1,9 @@
+function TestLayout({ children }) {
+  return (
+    <div className="flex w-full h-screen items-center justify-center">
+      {children}
+    </div>
+  );
+}
+
+export default TestLayout;
---
+++ app/(testroute)/org/page.jsx
@@ -0,0 +1,5 @@
+function Org() {
+  return <p>this is test route</p>;
+}
+
+export default Org;
